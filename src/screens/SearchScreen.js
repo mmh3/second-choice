e7537b0c5e2aa40041efcf1efa227d4bf56ba128
mmh <matt.hensinger@gmail.com>
2019-10-20 00:32:22 -0400

    Cleanup after moving searchApi method out to actions object.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,42 +1,14 @@
 import React, { useState } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import firebase from 'firebase';
 import ResultsList from '../components/ResultsList';
 import { search } from '../actions/ResultsActions';
 import { connect } from 'react-redux';
 
 function SearchScreen(props) {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async searchTerm => {
-    try {
-        resultsArray = [];
-
-        firebase.database().ref('/food').orderByChild('name').equalTo(term)
-          .once('value', snapshot => {
-            snapshot.forEach(function(childSnapshot) {
-              firebase.database().ref('/food').orderByChild('groupUid').equalTo(childSnapshot.val().groupUid)
-                .once('value', snapshot => {
-                  snapshot.forEach(function(childSnapshot) {
-                    var item = childSnapshot.val();
-                    item.key = childSnapshot.key;
-
-                    console.log(childSnapshot.val().name);
-                    resultsArray.push(item);
-                  });
-                  setResults(resultsArray);
-                });
-            });
-          });
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  console.log('results: ' + props.results);
   return (
     <>
       <SearchBar
